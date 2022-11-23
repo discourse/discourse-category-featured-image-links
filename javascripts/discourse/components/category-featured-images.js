82b4ea87c740634d1f2ef7caefbcf449439c53f5
Kris <kris.aubuchon@discourse.org>
2022-11-22 19:52:18 -0500

    UX: optionally hide topic list, additional styling (#1)

--- javascripts/discourse/components/category-featured-images.js
+++ javascripts/discourse/components/category-featured-images.js
@@ -1,11 +1,27 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { inject as controller } from "@ember/controller";
 
 export default class CategoryFeaturedImages extends Component {
   @service router;
+  @controller application;
 
   get shouldShow() {
     return this.router.currentRoute.name.includes("discovery.category");
+  }
+
+  get hideTopicList() {
+    let categoryIDs = settings.hide_topic_list
+      .split("|")
+      .map((id) => parseInt(id, 10));
+    let currentCategoryID = this?.args?.category?.id;
+
+    if (categoryIDs?.includes(currentCategoryID)) {
+      // if we hide the topic list, we need to force the footer to show
+      this.application.showFooter = true;
+    }
+
+    return categoryIDs.includes(currentCategoryID);
   }
 
   get filteredSetting() {
@@ -17,6 +33,10 @@ export default class CategoryFeaturedImages extends Component {
       if (Object.keys(filter).length) {
         filteredSetting.push(filter);
       }
+    });
+
+    filteredSetting.forEach((filter) => {
+      filter.slug = filter.section_title.toLowerCase().replace(/\s/g, "");
     });
 
     let currentCategoryID = this?.args?.category?.id;
